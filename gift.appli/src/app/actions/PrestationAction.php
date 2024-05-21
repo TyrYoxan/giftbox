bac352e56278cefb8be3e092c62ceb81a1eb3fe4
tyryoxan <clem.bri25@gmail.com>
2024-05-21 18:01:53 +0200

    end TD3

--- gift.appli/src/app/actions/PrestationAction.php
+++ gift.appli/src/app/actions/PrestationAction.php
@@ -5,19 +5,20 @@ namespace gift\appli\app\actions;
 use gift\appli\models\Prestation;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpBadRequestException;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class PrestationAction
 {
     function __invoke(Request $request, Response $response, array $args): Response {
         $response = $response->withStatus(200);
-        $request = $request->getQueryParams()['id']?? null;
-        if($request == null){
-            return $response->withStatus(400);
+        $req = $request->getQueryParams()['id']?? null;
+        if($req == null){
+            throw new HttpBadRequestException($request, "id prestation manquant");
         }
         try{
             $prestation = new Prestation();
-            $prest = $prestation::where('id', '=', $request)->firstOrFail();
+            $prest = $prestation::where('id', '=', $req)->firstOrFail();
             $html = <<<HTML
                 <html lang="fr">
                 <body><h1>Prestation {$prest->libelle}</h1></body>
